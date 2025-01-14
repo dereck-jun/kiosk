06a1d9ce759813ce6d58f50fc635cba7964b2b6e
dereck-jun <yjpark0805@naver.com>
2025-01-14 10:09:02 +0900

    feat: 캡슐화 적용

---
+++ src/main/java/com/example/lv5/kiosk/Kiosk.java
@@ -0,0 +1,95 @@
+package com.example.lv5.kiosk;
+
+import com.example.lv5.menu.Menu;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    private final List<Menu> menus;
+
+    public Kiosk(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printMainMenu();
+            int selectMenu = scanner.nextInt();
+
+            if (inputValidation(selectMenu)) {
+                break;
+            }
+
+            switch (selectMenu) {
+                case 1 -> handleBurgerSelection(scanner);
+                case 2 -> handleDrinkSelection(scanner);
+                case 3 -> handleDessertSelection(scanner);
+                default -> throw new IllegalArgumentException("잘못된 선택입니다.");
+            }
+        }
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+    private void printMainMenu() {
+        System.out.println("[ MAIN MENU ]");
+
+        for (int i = 0; i < menus.size(); i++) {
+            System.out.println((i + 1) + ". " + menus.get(i).getCategory());
+        }
+
+        System.out.println("0. 종료");
+    }
+
+    private void handleBurgerSelection(Scanner scanner) {
+        Menu burgers = menus.get(0);
+        printMenuItems(burgers);
+        int selectedItem = scanner.nextInt();
+
+        if (inputValidation(selectedItem)) {
+            return;
+        }
+        burgers.printSelectedItem(selectedItem);
+    }
+
+    private void handleDrinkSelection(Scanner scanner) {
+        Menu drinks = menus.get(1);
+        printMenuItems(drinks);
+        int selectedItem = scanner.nextInt();
+
+        if (inputValidation(selectedItem)) {
+            return;
+        }
+        drinks.printSelectedItem(selectedItem);
+    }
+
+    private void handleDessertSelection(Scanner scanner) {
+        Menu desserts = menus.get(2);
+        printMenuItems(desserts);
+        int selectedItem = scanner.nextInt();
+
+        if (inputValidation(selectedItem)) {
+            return;
+        }
+        desserts.printSelectedItem(selectedItem);
+    }
+
+    private static void printMenuItems(Menu menu) {
+        System.out.println("[ " + menu.getCategory().toUpperCase() +" MENU ]");
+        for (int i = 0; i < menu.getMenuItems().size(); i++) {
+            System.out.println((i + 1) + ". " + menu.getMenuItems().get(i));
+        }
+        System.out.println("0. 뒤로가기");
+    }
+
+    private static boolean inputValidation(int selectMenu) {
+        if (selectMenu == 0) {
+            return true;
+        } else if (selectMenu < 0) {
+            throw new IllegalArgumentException("잘못된 선택입니다.");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/example/lv5/menu/MenuItem.java
@@ -0,0 +1,30 @@
+package com.example.lv5.menu;
+
+public class MenuItem {
+    private final String menuName;
+    private final double price;
+    private final String description;
+
+    public MenuItem(String menuName, double price, String description) {
+        this.menuName = menuName;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return menuName + " | W " + price + " | " + description;
+    }
+}
