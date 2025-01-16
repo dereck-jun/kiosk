fc5fce626deaa0857b8f45b12c09a8c6954fb810
dereck-jun <yjpark0805@naver.com>
2025-01-16 18:20:19 +0900

    refactor: 예외 처리 추가 및 변경 init 데이터 추가 방식 변경

--- src/main/java/com/example/lv4/menu/Menu.java
+++ src/main/java/com/example/lv4/menu/Menu.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
-    public String category;
-    public List<MenuItem> menuItems = new ArrayList<>();
+    public final String category;
+    public final List<MenuItem> menuItems = new ArrayList<>();
 
     public Menu(String category) {
         this.category = category;
@@ -14,6 +14,10 @@ public class Menu {
     public void printSelectedItem(int selectedItem) {
         MenuItem menuItem = menuItems.get(selectedItem - 1);
         System.out.println("선택한 메뉴: " + menuItem.menuName.trim() + " | W " + menuItem.price + " | " + menuItem.description);
+    }
+
+    public void addMenuItems(MenuItem... menuItem) {
+        menuItems.addAll(List.of(menuItem));
     }
 
     public String getCategory() {
--- src/main/java/com/example/lv4/menu/MenuItem.java
+++ src/main/java/com/example/lv4/menu/MenuItem.java
@@ -1,9 +1,9 @@
 package com.example.lv4.menu;
 
 public class MenuItem {
-    public String menuName;
-    public double price;
-    public String description;
+    public final String menuName;
+    public final double price;
+    public final String description;
 
     public MenuItem(String menuName, double price, String description) {
         this.menuName = menuName;
