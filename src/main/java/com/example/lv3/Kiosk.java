0fe355914473a1b0bb4e1a2ad580af50f36ce945
dereck-jun <yjpark0805@naver.com>
2025-01-16 18:48:04 +0900

    refactor: Scanner 리소스 해제

--- src/main/java/com/example/lv3/Kiosk.java
+++ src/main/java/com/example/lv3/Kiosk.java
@@ -34,6 +34,7 @@ public class Kiosk {
             printSelectedMenu(selectNum);
         }
         System.out.println("프로그램을 종료합니다.");
+        scanner.close();
     }
 
     private void printMainMenu() {
